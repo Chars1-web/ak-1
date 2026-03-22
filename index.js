a8920d3a25e1c85740bca80e72372bfbaec0b304
Chars1-web <realrax12345@gmail.com>
2026-03-22 10:48:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import Papa from "papaparse";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const https://docs.google.com/spreadsheets/d/e/2PACX-1vSe6mW01Q2kgWDNkm-= "https://docs.google.com/spreadsheets/d/e/2PACX-1vSe6mW01Q2kgWDNkm-WDUtJziEmKHcpvvP-TKHy84jTjTXE_qIjEQZPDDPb36Xqn5k8t5-kpB6ipq1N/pub?gid=712264809&single=true&output=csv"; // Google Sheet CSV link
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSe6mW01Q2kgWDNkm-WDUtJziEmKHcpvvP-TKHy84jTjTXE_qIjEQZPDDPb36Xqn5k8t5-kpB6ipq1N/pub?gid=712264809&single=true&output=csv"; // Google Sheet CSV link
 
 app.get("/api/sheet", async (req, res) => {
-  const response = await fetch(https://docs.google.com/spreadsheets/d/e/2PACX-1vSe6mW01Q2kgWDNkm-);
+  const response = await fetch(SHEET_CSV_URL);
   const csvText = await response.text();
   const parsed = Papa.parse(csvText, { header: true });
   res.json(parsed.data); // returns JSON array
